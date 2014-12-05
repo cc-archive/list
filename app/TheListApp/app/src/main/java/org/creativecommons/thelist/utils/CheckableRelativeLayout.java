3b2e13f200c45b292b88bb6288a5b6d3808f6b58
perpetuatingcuriosity <perpetuatingcuriosity@gmail.com>
2014-12-05 16:59:07 -0500

    2.3.7 list fix in progress

---
+++ app/TheListApp/app/src/main/java/org/creativecommons/thelist/utils/CheckableRelativeLayout.java
@@ -0,0 +1,75 @@
+package org.creativecommons.thelist.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Checkable;
+import android.widget.CheckedTextView;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by damaris on 2014-12-05.
+ */
+public class CheckableRelativeLayout extends RelativeLayout implements Checkable {
+
+    private boolean checked = false;
+
+    public CheckableRelativeLayout(Context context) {
+        super(context, null);
+    }
+
+    public CheckableRelativeLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    private static final int[] CheckedStateSet = {
+            android.R.attr.state_checked
+    };
+
+    @Override
+    protected void dispatchSetPressed(boolean pressed)
+    {
+        super.dispatchSetPressed(pressed);
+        setChecked(pressed);
+    }
+
+    @Override
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+        for (int index = 0; index < getChildCount(); index++)
+        {
+            View view = getChildAt(index);
+            if (view.getClass().toString().equals(CheckedTextView.class.toString()))
+            {
+                CheckedTextView checkable = (CheckedTextView)view;
+                checkable.setChecked(checked);
+                checkable.refreshDrawableState();
+            }
+        }
+        refreshDrawableState();
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void toggle() {
+        checked = !checked;
+        refreshDrawableState();
+    }
+
+    @Override
+    protected int[] onCreateDrawableState(int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(drawableState, CheckedStateSet);
+        }
+        return drawableState;
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+}
+
