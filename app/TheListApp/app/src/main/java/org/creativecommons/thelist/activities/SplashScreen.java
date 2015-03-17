7bed9a7a98f9279cdeccdfe010bed5b2f50bf5e5
Matt Lee <mattl@cnuk.org>
2015-03-17 14:22:25 -0400

    Updated

--- app/TheListApp/app/src/main/java/org/creativecommons/thelist/activities/SplashScreen.java
+++ app/TheListApp/app/src/main/java/org/creativecommons/thelist/activities/SplashScreen.java
@@ -21,7 +21,7 @@ public class SplashScreen extends Activity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                Intent i = new Intent(SplashScreen.this, StartActivity.class);
                 startActivity(i);
 
                 // close this activity
