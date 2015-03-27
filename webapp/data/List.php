4bc91ed5d43a52767ef49a3f6fa61246076ff36c
Matt Lee <mattl@cnuk.org>
2015-03-27 11:07:56 -0400

    return a lot more information

--- webapp/data/List.php
+++ webapp/data/List.php
@@ -132,7 +132,7 @@ class UserList {
 
         global $adodb;
 
-        $query = "SELECT url FROM Photos WHERE userid=?";
+        $query = 'SELECT p.*,l.* From Photos p LEFT JOIN List l on (p.listid = l.id) WHERE p.userid = ?';
         $params = array();
         $params[] = $userid;
 
