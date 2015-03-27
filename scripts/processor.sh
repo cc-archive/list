3532a70ac4268e6e5cb9a07603194e6a3f6f90c2
Matt Lee <mattl@cnuk.org>
2015-03-27 15:33:18 -0400

    Some scripts used in processing and serving images

---
+++ scripts/processor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash                                                                                                                                                                                                                                                                               
+
+for f in *.jpg
+do
+    if [ ! -f 200/$f ]
+    then
+        if [[ $(cc-xmp-tag --read $f ALL) == "" ]]
+        then
+            cc-xmp-tag --write $f License='BY'
+        fi
+        #convert -define jpeg:size=200x200 $f -thumbnail 200x200^ -gravity center -extent 200x200 200/$f                                                                                                                                                                                  
+        convert $f -thumbnail 200x200^ -auto-orient -gravity center -extent 200x200 200/$f
+        for n in 300 400 600 800 1024
+        do
+            if [ ! -f $n/$f ]
+                then
+                convert $f -auto-orient -resize $n $n/$f
+            fi
+        done
+    fi
+
+don
