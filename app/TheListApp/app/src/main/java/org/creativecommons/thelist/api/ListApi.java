813909e966c99628187b4e4d504066b27fb65d70
perpetuatingcuriosity <perpetuatingcuriosity@gmail.com>
2015-09-17 16:38:44 -0400

    add missing licenses

--- app/TheListApp/app/src/main/java/org/creativecommons/thelist/api/ListApi.java
+++ app/TheListApp/app/src/main/java/org/creativecommons/thelist/api/ListApi.java
@@ -1,3 +1,23 @@
+/* The List powered by Creative Commons
+
+   Copyright (C) 2014, 2015 Creative Commons Corporation
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of either the GNU Affero General Public License or
+   the GNU General Public License as published by the
+   Free Software Foundation, either version 3 of the Licenses, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+   You should have received a copy of the GNU General Public License and
+   the GNU Affero General Public License along with this program.
+
+   If not, see <http://www.gnu.org/licenses/>.
+*/
+
 package org.creativecommons.thelist.api;
 
 import retrofit.RestAdapter;
